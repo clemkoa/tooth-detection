a8f4d36fe24b85acdaa724f9888526efa19cad6d
Clement Joudet <clement.joudet@gmail.com>
2018-03-02 23:04:10 +0100

    Building dataset

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import logging
 import io
 import PIL
+import pickle
 import imageio
 import numpy as np
 from PIL import Image
@@ -61,7 +62,82 @@ def maybe_pickle(data_folders, force=False):
         print('Unable to save data to', set_filename, ':', e)
   return dataset_names
 
+def merge_datasets(datasets, l):
+    results = np.concatenate(tuple(datasets))
+    labels = np.concatenate(tuple(l))
+    return results, labels
+
 train_folders = ['train/6max', 'train/6mand']
 test_folders = ['test/6max', 'test/6mand']
 train_datasets = maybe_pickle(train_folders)
 test_datasets = maybe_pickle(test_folders)
+
+datasets = []
+l = []
+for index, foldername in enumerate(train_datasets):
+    dataset = pickle.load(open(foldername, "rb"))
+    datasets.append(dataset)
+    l.append(np.array([index for a in dataset]))
+    print(l)
+    print(len(dataset), np.mean(dataset), np.std(dataset))
+    print(dataset.shape)
+
+results, labels = merge_datasets(datasets, l)
+print(results, labels)
+
+# image_size = 28
+# num_labels = 2
+# num_channels = 1 # grayscale
+# batch_size = 16
+# patch_size = 5
+# depth = 16
+# num_hidden = 64
+#
+# graph = tf.Graph()
+#
+# with graph.as_default():
+#
+#   # Input data.
+#   tf_train_dataset = tf.placeholder(
+#     tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+#   tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+#   tf_valid_dataset = tf.constant(valid_dataset)
+#   tf_test_dataset = tf.constant(test_dataset)
+#
+#   # Variables.
+#   layer1_weights = tf.Variable(tf.truncated_normal(
+#       [patch_size, patch_size, num_channels, depth], stddev=0.1))
+#   layer1_biases = tf.Variable(tf.zeros([depth]))
+#   layer2_weights = tf.Variable(tf.truncated_normal(
+#       [patch_size, patch_size, depth, depth], stddev=0.1))
+#   layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
+#   layer3_weights = tf.Variable(tf.truncated_normal(
+#       [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
+#   layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+#   layer4_weights = tf.Variable(tf.truncated_normal(
+#       [num_hidden, num_labels], stddev=0.1))
+#   layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+#
+#   # Model.
+#   def model(data):
+#     conv = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
+#     hidden = tf.nn.relu(conv + layer1_biases)
+#     conv = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
+#     hidden = tf.nn.relu(conv + layer2_biases)
+#     shape = hidden.get_shape().as_list()
+#     reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+#     hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+#     return tf.matmul(hidden, layer4_weights) + layer4_biases
+#
+#   # Training computation.
+#   logits = model(tf_train_dataset)
+#   loss = tf.reduce_mean(
+#     tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+#
+#   # Optimizer.
+#   optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+#
+#   # Predictions for the training, validation, and test data.
+#   train_prediction = tf.nn.softmax(logits)
+#   valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+#   test_prediction = tf.nn.softmax(model(tf_test_dataset))
