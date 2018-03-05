53b61dcc46c74fa1b5b98d51fda7d901745c0837
Clement Joudet <clement.joudet@gmail.com>
2018-03-05 09:45:36 +0100

    Better model

--- train.py
+++ train.py
@@ -13,10 +13,10 @@ image_width = 50
 image_height = 100
 pixel_depth = 255.0  # Number of levels per pixel.
 num_channels = 1 # grayscale
-batch_size = 16
+batch_size = 10
 batch_repeat = 32
-patch_size = 5
-depth = 20
+patch_size = 10
+depth = 16
 num_hidden = 64
 dropout = 0.8
 num_steps = 10001
@@ -27,7 +27,7 @@ def load_tooth(folder):
   dataset = np.ndarray(shape=(len(image_files), image_height, image_width),
                          dtype=np.float32)
   num_images = 0
-  test_size = 0.1
+  test_size = 0.2
   for image in image_files:
     image_file = os.path.join(folder, image)
     # print('image', image_file)
@@ -55,7 +55,7 @@ def maybe_pickle(data_folders, force=False):
   for folder in data_folders:
     set_filename = folder
     dataset_names.append(set_filename)
-    if os.path.exists(set_filename) and not force:
+    if os.path.exists(set_filename + 'train.pickle') and not force:
       # You may override by setting force=True.
       print('%s already present - Skipping pickling.' % set_filename)
     else:
@@ -120,7 +120,8 @@ def inception2d(x, in_channels, filter_count):
     print(x)
     return tf.nn.relu(x)
 
-train_folders = ['train/16', 'train/17', 'train/26', 'train/27', 'train/36', 'train/37', 'train/46', 'train/47']
+train_folders = ['train/11', 'train/12', 'train/13', 'train/14', 'train/15', 'train/16', 'train/17']
+#, 'train/27', 'train/36', 'train/37', 'train/46', 'train/47']
 dataset_names = maybe_pickle(train_folders)
 
 train_features, train_labels, test_features, test_labels = merge_datasets(dataset_names)
@@ -144,38 +145,45 @@ with graph.as_default():
         [patch_size, patch_size, num_channels, depth], stddev=0.1))
     layer1_biases = tf.Variable(tf.zeros([depth]))
     layer2_weights = tf.Variable(tf.truncated_normal(
-        [patch_size, patch_size, 61, 61], stddev=0.1))
-    layer2_biases = tf.Variable(tf.constant(1.0, shape=[61]))
+        [patch_size, patch_size, depth, 2 * depth], stddev=0.1))
+    layer2_biases = tf.Variable(tf.constant(1.0, shape=[2 * depth]))
     layer3_weights = tf.Variable(tf.truncated_normal(
-        [image_height // 4 * (image_width + 2) // 4 * 61, num_hidden], stddev=0.1))
-    layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+        [patch_size, patch_size, 2 * depth, 4 * depth], stddev=0.1))
+    layer3_biases = tf.Variable(tf.constant(1.0, shape=[4 * depth]))
     layer4_weights = tf.Variable(tf.truncated_normal(
+        [(image_height + 4) // 8 * (image_width + 6) // 8 * 4 * depth, num_hidden], stddev=0.1))
+    layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+    layer5_weights = tf.Variable(tf.truncated_normal(
         [num_hidden, num_labels], stddev=0.1))
-    layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
-
+    layer5_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
     # Model.
     def model(data):
-        conv1 = inception2d(data, 1, depth) # tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
-        # hidden1 = tf.nn.relu(conv1 + layer1_biases)
+        print(data)
+        # conv1 = inception2d(data, 1, depth)
+        conv1 = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+        hidden1 = tf.nn.relu(conv1 + layer1_biases)
         pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
         conv2 = tf.nn.conv2d(pool1, layer2_weights, [1, 1, 1, 1], padding='SAME')
         hidden2 = tf.nn.relu(conv2 + layer2_biases)
         pool2 = tf.nn.max_pool(hidden2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-        shape = pool2.get_shape().as_list()
-        reshape = tf.reshape(pool2, [-1, shape[1] * shape[2] * shape[3]])
-        hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+        conv3 = tf.nn.conv2d(pool2, layer3_weights, [1, 1, 1, 1], padding='SAME')
+        hidden3 = tf.nn.relu(conv3 + layer3_biases)
+        pool3 = tf.nn.max_pool(hidden3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        shape = pool3.get_shape().as_list()
+
+        reshape = tf.reshape(pool3, [-1, shape[1] * shape[2] * shape[3]])
+        hidden = tf.nn.relu(tf.matmul(reshape, layer4_weights) + layer4_biases)
         hidden = tf.nn.dropout(hidden, dropout)
-        return tf.matmul(hidden, layer4_weights) + layer4_biases
+        return tf.matmul(hidden, layer5_weights) + layer5_biases
 
     logits = model(next_element)
-    loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=next_label, logits=logits))
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=next_label, logits=logits))
     tf.summary.scalar('loss', loss)
 
     global_step = tf.Variable(0, trainable=False)
-    starter_learning_rate = 0.01
+    starter_learning_rate = 0.001
     learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, num_steps, 0.9)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
